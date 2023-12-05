4ba3872de6d6c658eaee7be0808f4ca9bf14a71a
DANTE <104479517+xflydan@users.noreply.github.com>
2023-12-06 05:24:11 +0700

    Update extras.py

--- BrandrdXMusic/utils/inline/extras.py
+++ BrandrdXMusic/utils/inline/extras.py
@@ -18,7 +18,7 @@ def close_markup(_):
         [
             [
                  InlineKeyboardButton(
-                text="⛦ ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
+                text=" ꜱᴜᴘᴘᴏʀᴛ ", url=f"https://t.me/dansupport"
             ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
